49053c4b2202f5158e45f07c21dfa8e67576340b
Hell077 <147360086+Hell077@users.noreply.github.com>
2024-02-22 03:30:50 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -5,14 +5,22 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/about', (req, res) => {
+app.get('/about.html', (req, res) => {
   res.render('about');
+});
+
+app.post('/check-user', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(`username ---  ${username}  password --- ${password}`)
+  res.redirect ('/')
 });
 
 

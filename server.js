cb0a759303fa93d0af6877a9463b21e29fd418e8
Hell077 <147360086+Hell077@users.noreply.github.com>
2024-02-22 21:58:26 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,31 +1,42 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const app = express();
+const { addUserToDB } = require('./public/js/database');
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/about.html', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 app.post('/check-user', (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  console.log(`username ---  ${username}  password --- ${password}`)
-  res.redirect ('/')
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // Проверка имени пользователя и пароля на пустоту
+    if (!username || !password) {
+        // Если имя пользователя или пароль пусты, показываем сообщение об ошибке
+        return res.status(400).send('Имя пользователя и пароль обязательны!');
+    }
+
+    // Добавление пользователя в базу данных
+    addUserToDB(username, password)
+        .then(() => {
+            console.log('Пользователь успешно добавлен в базу данных');
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.error('Ошибка при добавлении пользователя в базу данных:', error);
+            res.redirect('/');
+        });
 });
 
-
 const PORT = 3333;
-
 app.listen(PORT, () => {
     console.log(`Сервер работает по адресу: http://localhost:${PORT}`);
 });

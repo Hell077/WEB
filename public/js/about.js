325bd6be1db3fff9ccfef8dd03c35cb56b2b6517
Hell077 <147360086+Hell077@users.noreply.github.com>
2024-02-29 02:14:25 +0600

    Delete public directory

--- public/js/about.js
+++
@@ -1,5 +0,0 @@
-//Возвращение на главную со страницы about
-let backButton = document.querySelector('.back');
-backButton.addEventListener('click', function() {
-    window.location.href = '/';
-});
--- public/js/aboutin.js
+++
@@ -1,6 +0,0 @@
-// Вход на страницу about
-let about = document.querySelector('.aboutusbtn');
-about.addEventListener('click', function() {
-    window.location.href = './about.html';
-});
-
